1e6575d7024b3f915a399808dc77b5ddf1f0ff80
Lizzie Siegle <esiegle@brynmawr.edu>
2021-07-21 00:12:20 -0700

    cant detect incorrect mask usage when tried to train, think its best when binary

--- assets/index.js
+++ assets/index.js
@@ -5,12 +5,14 @@
     const Video = Twilio.Video;
     let videoRoom, localStream;
     const video = document.getElementById("video");
-    // const noMaskButton = document.getElementById('noMaskButton');
-    // const maskButton = document.getElementById('maskButton');
-    // const amountOfLabel1Images = document.getElementById('numNoMaskImages');
-    // const amountOfLabel2Images = document.getElementById('numMaskImages');
-    // const train = document.getElementById('train');
-    // const loss = document.getElementById('loss');
+    const noMaskButton = document.getElementById('noMaskButton');
+    const maskButton = document.getElementById('maskButton');
+    const incorrectMaskButton = document.getElementById('incorrectMaskButton');
+    const amountOfLabel1Images = document.getElementById('numNoMaskImages');
+    const amountOfLabel2Images = document.getElementById('numMaskImages');
+    const amountOfLabel3Images = document.getElementById('numIncorrectMaskImages');
+    const train = document.getElementById('train');
+    const loss = document.getElementById('loss');
     const resSpan = document.getElementById('result');
     const conf = document.getElementById('confidence');
     const predictButton = document.getElementById('predict');
@@ -52,26 +54,30 @@
                 classifier = featureExtractor.classification(video);
                 //buttons for when you need to build the model
                 //no mask
-                // noMaskButton.onclick = () => { 
-                //     classifier.addImage('no');
-                //     amountOfLabel1Images.innerText = Number(amountOfLabel1Images.innerText) + 1;
-                // };
-                // maskButton.onclick = () => { //mask
-                //     classifier.addImage('yes');
-                //     amountOfLabel2Images.innerText = Number(amountOfLabel2Images.innerText) + 1;
-                // };
-                // train.onclick = () => {
-                //     classifier.train((lossValue) => {
-                //         if (lossValue) {
-                //             totalLoss = lossValue;
-                //             loss.innerHTML = `Loss: ${totalLoss}`;
-                //         } 
-                //         else {
-                //             loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
-                //         }
-                //     });
-                //     //console.log(classifier);
-                // };
+                noMaskButton.onclick = () => { 
+                    classifier.addImage('no');
+                    amountOfLabel1Images.innerText = Number(amountOfLabel1Images.innerText) + 1;
+                };
+                maskButton.onclick = () => { //mask
+                    classifier.addImage('yes');
+                    amountOfLabel2Images.innerText = Number(amountOfLabel2Images.innerText) + 1;
+                };
+                incorrectMaskButton.onclick = () => { //incorrect mask
+                    classifier.addImage('incorrect');
+                    amountOfLabel3Images.innerText = Number(amountOfLabel3Images.innerText) + 1;
+                };
+                train.onclick = () => {
+                    classifier.train((lossValue) => {
+                        if (lossValue) {
+                            totalLoss = lossValue;
+                            loss.innerHTML = `Loss: ${totalLoss}`;
+                        } 
+                        else {
+                            loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
+                        }
+                    });
+                    //console.log(classifier);
+                };
                 const resultsFunc = (err, res) => {
                     if (err) {
                         console.error(err);
@@ -86,9 +92,9 @@
                 predictButton.onclick = () => {
                     classifier.classify(resultsFunc);
                 };
-                // saveModelButton.onclick = () => {
-                //     featureExtractor.save();
-                // }
+                saveModelButton.onclick = () => {
+                    featureExtractor.save();
+                }
             });
         });
     };
