ada57719624b2180e87c4b15048665085a1aca2b
Lizzie Siegle <esiegle@brynmawr.edu>
2021-07-22 11:38:10 -0700

    clean spacing, buttons, link btwn pages

--- assets/train.js
+++ assets/train.js
@@ -2,56 +2,56 @@ const video = document.getElementById("video");
 const resSpan = document.getElementById('result');
 const conf = document.getElementById('confidence');
 const saveModelButton = document.getElementById('save');
-    const noMaskButton = document.getElementById('noMaskButton');
-    const maskButton = document.getElementById('maskButton');
-    const amountOfLabel1Images = document.getElementById('numNoMaskImages');
-    const amountOfLabel2Images = document.getElementById('numMaskImages');
-    const predictButton = document.getElementById('predict');
-    let featureExtractor = ml5.featureExtractor('MobileNet'); 
-    const classifier = featureExtractor.classification(video);
-    let localStream, totalLoss;
-    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+const noMaskButton = document.getElementById('noMaskButton');
+const maskButton = document.getElementById('maskButton');
+const amountOfLabel1Images = document.getElementById('numNoMaskImages');
+const amountOfLabel2Images = document.getElementById('numMaskImages');
+const predictButton = document.getElementById('predict');
+const featureExtractor = ml5.featureExtractor('MobileNet'); 
+const classifier = featureExtractor.classification(video);
+let localStream, totalLoss;
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
     .then(vid => {
         video.srcObject = vid;
         localStream = vid;
     })
-//buttons for when you need to build the model
-                //no mask
-                noMaskButton.onclick = () => { 
-                    classifier.addImage('no');
-                    amountOfLabel1Images.innerText = Number(amountOfLabel1Images.innerText) + 1;
-                };
-                maskButton.onclick = () => { //mask
-                    classifier.addImage('yes');
-                    amountOfLabel2Images.innerText = Number(amountOfLabel2Images.innerText) + 1;
-                };
-                train.onclick = () => {
-                    classifier.train((lossValue) => {
-                        if (lossValue) {
-                            totalLoss = lossValue;
-                            loss.innerHTML = `Loss: ${totalLoss}`;
-                        } 
-                        else {
-                            loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
-                        }
-                    });
-                    //console.log(classifier);
-                };
-                const resultsFunc = (err, res) => {
-                    if (err) {
-                        console.error(err);
-                    } 
-                    else if (res && res[0]) {
-                        resSpan.innerText = res[0].label;
-                        conf.innerText = res[0].confidence;
-                        classifier.classify(resultsFunc); // recall the classify function again
-                        //console.dir(classifier);
-                    }
-                }
-                predictButton.onclick = () => {
-                    classifier.classify(resultsFunc);
-                };
-                 saveModelButton.onclick = () => {
-                    featureExtractor.save();
-                }
+    //buttons for when you need to build the model
+    //no mask
+    noMaskButton.onclick = () => { 
+        classifier.addImage('no');
+        amountOfLabel1Images.innerText = Number(amountOfLabel1Images.innerText) + 1;
+    };
+    maskButton.onclick = () => { //mask
+        classifier.addImage('yes');
+        amountOfLabel2Images.innerText = Number(amountOfLabel2Images.innerText) + 1;
+    };
+    train.onclick = () => {
+        classifier.train((lossValue) => {
+            if (lossValue) {
+                totalLoss = lossValue;
+                loss.innerHTML = `Loss: ${totalLoss}`;
+            } 
+            else {
+                loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
+            }
+        });
+        //console.log(classifier);
+    };
+    const resultsFunc = (err, res) => {
+        if (err) {
+            console.error(err);
+        } 
+        else if (res && res[0]) {
+            resSpan.innerText = res[0].label;
+            conf.innerText = res[0].confidence;
+            classifier.classify(resultsFunc); // recall the classify function again
+            //console.dir(classifier);
+        }
+    }
+    predictButton.onclick = () => {
+        classifier.classify(resultsFunc);
+    };
+    saveModelButton.onclick = () => {
+        featureExtractor.save();
+    }
                 
